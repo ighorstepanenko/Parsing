5d483da83ed0eb2bfe4aee2133364ccaf4858dbf
ighorstepanenko <ighor_stepanenko@list.ru>
2022-12-01 23:35:14 +0300

    Version2

--- Parsing.py
+++ Parsing.py
@@ -3,55 +3,30 @@ import hashlib
 from urllib.request import urlopen, Request
 from playsound import playsound
 
-# setting the URL you want to monitor
 url = Request("https://biggeek.ru/catalog/apple-airpods",
               headers={"User-Agent": "Mozilla/5.0"})
 
-# to perform a GET request and load the
-# content of the website and store it in a var
 response = urlopen(url).read()
 
-# to create the initial hash
+
 currentHash = hashlib.sha224(response).hexdigest()
 print("running")
 while True:
     try:
-        # perform the get request and store it in a var
         response = urlopen(url).read()
-
-        # create a hash
         currentHash = hashlib.sha224(response).hexdigest()
-
-        # wait for 30 seconds
         time.sleep(20)
-
-        # perform the get request
         response = urlopen(url).read()
-
-        # create a new hash
         newHash = hashlib.sha224(response).hexdigest()
-
-        # check if new hash is same as the previous hash
         if newHash == currentHash:
             continue
-
-        # if something changed in the hashes
         else:
-            # notify
             for _ in range(5):
                 playsound('/home/igor/Downloads/budilnik.mp3')
             print("something changed")
-
-            # again read the website
             response = urlopen(url).read()
-
-            # create a hash
             currentHash = hashlib.sha224(response).hexdigest()
-
-            # wait for 30 seconds
             time.sleep(30)
             continue
-
-    # To handle exceptions
     except Exception as e:
         print("error")
